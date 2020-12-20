45c44ee899ebbfa3cee9e1b6103266b888f5c979
Arnold Dziudzik <arn.dziudzik@gmail.com>
2020-12-20 16:01:06 +0200

    progress: added delete buttons for each item on the project list

--- src/modules/render.js
+++ src/modules/render.js
@@ -2,7 +2,6 @@ import createElement from '../utils/element-creator';
 import appendElement from '../utils/element-appender';
 import selectElement from '../utils/element-selector';
 import addListenerProjectListItem from '../listeners/project-list-item';
-import addListenerCancelProjectBtn from '../listeners/cancel-project-btn';
 
 const renderProjects = (projectsArray) => {
     console.log('rendering projects...')
@@ -16,11 +15,19 @@ const renderProjects = (projectsArray) => {
         const projectDiv = createElement(
             'li', 
             `project-list-item-${i}`, 
-            'project-list-item',
-            project.getProjectName()
+            'project-list-item'
         );
-        //const deleteProjectBtn = createElement('button', `delete-project-btn-${i+1}`);
+        console.log('creating delete-project-btn...')
+        const listItemName = createElement('span', `item-list-name--${i+1}`, 'item-list-name', project.getProjectName());
+        const deleteProjectBtn = createElement('div', `delete-project-btn-${i+1}`, 'delete-project-btn');
+        const deleteProjectIcon = createElement('span', `delete-project-icon-${i+1}`, 'material-icons', 'delete');
+        deleteProjectIcon.classList.add('delete-project-icon');
+        console.log('delete-project-btn created!')
+        console.log('appendingn delete button');
         appendElement('projects-list', projectDiv);
+        appendElement(`project-list-item-${i}`, listItemName);
+        appendElement(`project-list-item-${i}`, deleteProjectBtn);
+        appendElement(`delete-project-btn-${i+1}`, deleteProjectIcon);
         console.log('project appended!');
         console.log('adding listener...')
         addListenerProjectListItem(`project-list-item-${i}`);
